d423ba20a4a436dcba692a2691b57e97eacf9fc6
Jamie Wilkinson <jamie@jamiedubs.com>
2016-07-08 09:45:49 -0400

    This duplicates bug was fixed in git master

--- customers.rb
+++ customers.rb
@@ -19,17 +19,8 @@ product_href = products.last.href
 customers = Vhx::Customer.all(product: product_href)
 puts "Found #{customers.length} customers for #{product_href}"
 
-# FIXME @sgrshah customers() call is returning lots of duplicate records
-# getting 500+ entries when I expect ~5
-
-real_customers = []
 customers.each do |c|
-  # puts "#{c.id}: #{c.email} #{c.name.inspect}"
-  real_customers << c.email unless real_customers.include?(c.email)
+  puts "#{c.id}: #{c.email} #{c.name.inspect}"
 end
 
-puts "#{real_customers.length} real customers"
-puts real_customers.inspect
-
-
-
+puts "All done"
